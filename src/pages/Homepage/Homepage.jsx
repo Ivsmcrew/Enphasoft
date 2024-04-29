d353b20559aa7d2618191f9a5ff346819c08562d
Ivsmcrew <Ivsmcrew@gmail.com>
2024-04-29 19:13:00 +0300

    feat: add start page

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,7 +4,10 @@ import styles from './Homepage.module.css'
 function Homepage() {
   return (
     <main className={styles.main}>
-      Homepage
+      <section className={styles.content}> 
+        <p className={styles.first}>EMPHA</p>
+        <p className={styles.second}>SOFT</p>
+      </section>
     </main>
   )
 }
