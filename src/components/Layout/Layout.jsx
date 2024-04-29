0365ccb6290985266267354a100461c4db343d21
Ivsmcrew <Ivsmcrew@gmail.com>
2024-04-29 16:22:14 +0300

    feat: create structure components

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../UI/sections/Navbar/Navbar"
+import Footer from "../../UI/sections/Footer/Footer"
 
 const Layout = function() {
   return(
     <>
-      <IvsmNavbar />
+      <Navbar />
       <Outlet />
       <Footer />
     </>
--- src/router/router.js
+++ src/router/router.js
@@ -1,3 +1,9 @@
+import Homepage from '../pages/Homepage/Homepage'
+import Login from '../pages/Login/Login'
+import Users from '../pages/Users/Users'
+import User from '../pages/User/User'
+import ErrorPage from '../pages/ErrorPage/ErrorPage'
+
 export const privateRoutes = [
   {path: null, element: Homepage, index: true},
   {path: 'login', element: Login, index: false},
