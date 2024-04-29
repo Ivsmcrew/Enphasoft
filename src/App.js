e3e508898d5b6020f06e558e1230a861a2cdbe36
Ivsmcrew <Ivsmcrew@gmail.com>
2024-04-29 16:25:40 +0300

    feat: add reset styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import AppRouter from "./components/AppRouter/AppRouter";
 import AuthProvider from "./components/Provider/AuthProvider";
 import TokenProvider from "./components/Provider/TokenProvider";
+import "./styles/App.css"
 
 function App() {
   return (
