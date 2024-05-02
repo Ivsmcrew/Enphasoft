ab6125c228c034686afa9f9ef3514686c475f0ff
Ivsmcrew <Ivsmcrew@gmail.com>
2024-05-02 22:31:47 +0300

    feat: add styles for edit form

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -1,12 +1,11 @@
 import { ErrorMessage, Field, Form, Formik, withFormik } from 'formik';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import * as helper from './helper'
 import styles from "./EditForm.module.css"
 import CmButton from '../../UI/buttons/CmButton/CmButton';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-function EditForm({submitFunction}) {
-  const dispatch = useDispatch();
+function EditForm({submitFunction, isActive}) {
   const viewedUser = useSelector(state => state.users.viewedUser);
 
   return (
@@ -32,59 +31,61 @@ function EditForm({submitFunction}) {
       enableReinitialize
     >
       {() => (  //errors, touched and other usefull methods of useFormik
-        <Form className={styles.form}>
-          <div className={styles.input}>
-            <label htmlFor="username" className={styles.label}>Username</label>
-            <Field 
-              id="username"
-              name="username" 
-              placeholder="your username" 
-              validate={ helper.validateUsername } 
-              className={styles.field}
-            />
-            <ErrorMessage name="username" render={msg => <div className={styles.error}>{msg}</div>} />
-          </div>
+        <Form className={isActive ? styles.form : `${styles.form} ${styles.form_inactive}`}>
+          <fieldset className={styles.activeWrapper} disabled={!isActive}>
+            <div className={styles.input}>
+              <label htmlFor="username" className={styles.label}>Username</label>
+              <Field 
+                id="username"
+                name="username" 
+                placeholder="your username" 
+                validate={ helper.validateUsername } 
+                className={styles.field}
+              />
+              <ErrorMessage name="username" render={msg => <div className={styles.error}>{msg}</div>} />
+            </div>
 
-          <div className={styles.input}>
-            <label htmlFor="first_name" className={styles.label}>First name</label>
-            <Field 
-              id="first_name"
-              name="first_name" 
-              placeholder="your first name" 
-              validate={ helper.validateName } 
-              className={styles.field}
-            />
-            <ErrorMessage name="first_name" render={msg => <div className={styles.error}>{msg}</div>} />
-          </div>
-         
-          <div className={styles.input}>
-            <label htmlFor="last_name" className={styles.label}>Last name</label>
-            <Field 
-              id="last_name"
-              name="last_name" 
-              placeholder="your last name" 
-              validate={ helper.validateName } 
-              className={styles.field}
-            />
-            <ErrorMessage name="last_name" render={msg => <div className={styles.error}>{msg}</div>} />
-          </div>
-         
-          <div className={styles.input}>
-            <label htmlFor="password" className={styles.label}>Password</label>
-            <Field 
-              id="password"
-              type="password"
-              name="password" 
-              placeholder="your password" 
-              validate={ helper.validatePassword } 
-              className={styles.field}
-            />
-            <ErrorMessage name="password" render={msg => <div className={styles.error}>{msg}</div>} />
-          </div>
+            <div className={styles.input}>
+              <label htmlFor="first_name" className={styles.label}>First name</label>
+              <Field 
+                id="first_name"
+                name="first_name" 
+                placeholder="your first name" 
+                validate={ helper.validateName } 
+                className={styles.field}
+              />
+              <ErrorMessage name="first_name" render={msg => <div className={styles.error}>{msg}</div>} />
+            </div>
+          
+            <div className={styles.input}>
+              <label htmlFor="last_name" className={styles.label}>Last name</label>
+              <Field 
+                id="last_name"
+                name="last_name" 
+                placeholder="your last name" 
+                validate={ helper.validateName } 
+                className={styles.field}
+              />
+              <ErrorMessage name="last_name" render={msg => <div className={styles.error}>{msg}</div>} />
+            </div>
+          
+            <div className={styles.input}>
+              <label htmlFor="password" className={styles.label}>Password</label>
+              <Field 
+                id="password"
+                type="password"
+                name="password" 
+                placeholder="your password" 
+                validate={ helper.validatePassword } 
+                className={styles.field}
+              />
+              <ErrorMessage name="password" render={msg => <div className={styles.error}>{msg}</div>} />
+            </div>
 
-          <CmButton type="submit">
-            Save changes
-          </CmButton>
+            <CmButton type="submit">
+              Save changes
+            </CmButton>
+          </fieldset>
         </Form>
       )}
     </Formik>
--- src/components/ViewField/ViewField.jsx
+++ src/components/ViewField/ViewField.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ViewField.module.css'
 import CmButton from "../../UI/buttons/CmButton/CmButton"
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,6 +10,7 @@ function ViewField() {
   const dispatch = useDispatch();
   const viewedUser = useSelector(state => state.users.viewedUser);
   const token = useSelector(state => state.auth.token);
+  const [isActive, setIsActive] = useState(false);
 
   async function deleteHandler() {
     if (!viewedUser) return
@@ -40,10 +41,12 @@ function ViewField() {
 
   return (
     <div className={styles.users__viewField}>
-      <CmButton>Edit</CmButton>
+      <CmButton onClick={() => setIsActive(!isActive)}>
+        {isActive ? "Discard" : "Edit"}
+      </CmButton>
       <CmButton onClick={deleteHandler}>Delete</CmButton>
 
-      <EditForm submitFunction={updateUser}/>
+      <EditForm submitFunction={updateUser} isActive={isActive}/>
     </div>
   )
 }
